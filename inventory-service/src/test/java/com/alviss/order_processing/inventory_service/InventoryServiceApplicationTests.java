375af0934ecadb7794c4411d1e0e98ca950cefb9
Alviss Raghnall <isiomastewart@gmail.com>
2025-06-03 05:49:34 +0100

    Initial commit

---
+++ inventory-service/src/test/java/com/alviss/order_processing/inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alviss.order_processing.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
